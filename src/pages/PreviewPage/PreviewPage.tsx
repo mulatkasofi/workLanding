2017782127fb75306e1b612861a4ba59cf30567c
Sofi <myfamily.sofi@gmail.com>
2024-10-07 00:06:50 +0200

    fix/ language change

--- src/pages/PreviewPage/PreviewPage.tsx
+++ src/pages/PreviewPage/PreviewPage.tsx
@@ -1,26 +1,32 @@
-'use client';
+"use client";
 
-import styles from './PreviewPage.module.css';
-import userPhoto from '../../img/viktoria.png';
-import Image from 'next/image';
-import cn from 'classnames';
-import fireIcon from '../../img/fire.png';
-import { useState } from 'react';
-import roundIcon from '../../img/элипсы фон 1 экран.png';
-import Header from '../../components/Header/Header';
-import ToggleSwitch from '../../components/ToggleSwitch/ToggleSwitch';
-import { useTranslation } from 'react-i18next';
-import fireIconAdaptive from '../../img/fire (1).png';
+import styles from "./PreviewPage.module.css";
+import userPhoto from "../../img/viktoria.png";
+import Image from "next/image";
+import cn from "classnames";
+import fireIcon from "../../img/fire.png";
+import { useEffect, useState } from "react";
+import roundIcon from "../../img/элипсы фон 1 экран.png";
+import Header from "../../components/Header/Header";
+import ToggleSwitch from "../../components/ToggleSwitch/ToggleSwitch";
+import { useTranslation } from "react-i18next";
+import fireIconAdaptive from "../../img/fire (1).png";
 
 const PreviewPage = () => {
   const { t, i18n } = useTranslation();
   const [switchState, setSwitchState] = useState(false);
 
   const handleSwitchChange = (
-    newState: boolean | ((_prevState: boolean) => boolean),
+    newState: boolean | ((_prevState: boolean) => boolean)
   ) => {
     setSwitchState(newState);
   };
+  
+  useEffect(() => {
+    if (!i18n.language) {
+      i18n.changeLanguage("ru");
+    }
+  }, [i18n]);
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -30,18 +36,18 @@ const PreviewPage = () => {
     <div className={styles.languageBlock}>
       <button
         className={cn(styles.languageText, {
-          [styles.languageTextCLick]: i18n.language === 'ru',
+          [styles.languageTextCLick]: i18n.language === "ru",
         })}
-        onClick={() => changeLanguage('ru')}
+        onClick={() => changeLanguage("ru")}
       >
         ru
       </button>
       <p className={styles.infoElementText}>/</p>
       <button
         className={cn(styles.languageText, {
-          [styles.languageTextCLick]: i18n.language === 'en',
+          [styles.languageTextCLick]: i18n.language === "en",
         })}
-        onClick={() => changeLanguage('en')}
+        onClick={() => changeLanguage("en")}
       >
         en
       </button>
@@ -51,12 +57,12 @@ const PreviewPage = () => {
   const renderInfoElements = () => (
     <div>
       <p className={styles.infoElementText}>
-        <span className={styles.infoElementSpan}>{'>'}</span>{' '}
-        {t('preview.experience')}
+        <span className={styles.infoElementSpan}>{">"}</span>{" "}
+        {t("preview.experience")}
       </p>
       <p className={styles.infoElementText}>
-        <span className={styles.infoElementSpan}>{'>'}</span>{' '}
-        {t('preview.layouts')}
+        <span className={styles.infoElementSpan}>{">"}</span>{" "}
+        {t("preview.layouts")}
       </p>
     </div>
   );
@@ -69,7 +75,7 @@ const PreviewPage = () => {
         className={cn(styles.previewPageImage, {
           [styles.showImage]: switchState,
         })}
-        style={{ color: 'white', width: '100%' }}
+        style={{ color: "white", width: "100%" }}
       />
 
       <div className={cn(styles.fireIcon, { [styles.animate]: switchState })}>
@@ -89,54 +95,54 @@ const PreviewPage = () => {
         <Header />
         <div className={styles.languageAdaptive}>{renderLanguageBlock()}</div>
         <h1 className={styles.previewPageTitle}>
-          {t('preview.greeting')}{' '}
+          {t("preview.greeting")}{" "}
           <span className={styles.previewPageSpan}>
-            {t('preview.jobTitle')}
+            {t("preview.jobTitle")}
             <span className={styles.userImageAdaptive}>
               <Image
                 src={userPhoto}
-                alt={''}
+                alt={""}
                 className={cn(styles.userPhotoAdaptive, {
                   [styles.rotate]: switchState,
                 })}
-              />{' '}
-              {t('preview.name')}
+              />{" "}
+              {t("preview.name")}
             </span>
           </span>
           <div className={styles.previewPageBlock}>
             <span className={styles.previewPageSpanName}>
-              {t('preview.name')}
+              {t("preview.name")}
             </span>
             <Image
               src={userPhoto}
-              alt={''}
+              alt={""}
               className={cn(styles.userPhoto, {
                 [styles.rotate]: switchState,
               })}
             />
 
             <span className={styles.lovesPerfectAdaptive}>
-              {t('preview.lovesPerfectAdaptive')}
+              {t("preview.lovesPerfectAdaptive")}
             </span>
             <span className={styles.lovesPerfect}>
-              {t('preview.lovesPerfect')}
+              {t("preview.lovesPerfect")}
             </span>
 
             <div className={styles.previewPageSpanRight}>
               <span className={styles.andComfortable}>
-                {' '}
-                {t('preview.andComfortable')}
+                {" "}
+                {t("preview.andComfortable")}
               </span>
-              {i18n.language === 'ru' && (
+              {i18n.language === "ru" && (
                 <span className={styles.andComfortableRU}>
-                  {t('preview.andComfortableAdaptive')}
+                  {t("preview.andComfortableAdaptive")}
                 </span>
               )}
               <div className={styles.previewPageInfoBlock}>
                 <div className={styles.infoElements}>
                   {renderInfoElements()}
                 </div>
-                {t('preview.design')}
+                {t("preview.design")}
                 <div className={styles.infoElementsAdaptive}>
                   {renderInfoElements()}
                 </div>
