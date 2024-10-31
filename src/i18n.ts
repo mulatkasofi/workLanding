b4a1685f5bce4ecdec80a6936ebabc97bb3d8178
Sofi <myfamily.sofi@gmail.com>
2024-10-31 17:14:29 +0100

    fix/ ui bugs

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,26 +2,28 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationRU from "../public/locales/ru/translation.json";
 import translationEN from "../public/locales/en/translation.json";
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: translationEN,
-      },
-      ru: {
-        translation: translationRU,
-      },
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: translationEN,
     },
-    lng: "en",
-    fallbackLng: "en",
-    debug: true,
-    interpolation: {
-      escapeValue: false,
+    ru: {
+      translation: translationRU,
     },
-    react: {
-      useSuspense: false,
-    },
-  });
+  },
+  lng: "en",
+  fallbackLng: "en",
+  detection: {
+    order: ["querystring", "cookie", "localStorage", "navigator"],
+    lookupQuerystring: "lng",
+  },
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
 
 export default i18n;
